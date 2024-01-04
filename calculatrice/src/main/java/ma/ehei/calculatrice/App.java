14742596daa6d91acef69b145183195bbfe8ab89
Aissaoui Oussama <aissaouioussama712@gmail.com>
2024-01-04 16:05:19 +0100

    la fonction de multiplication

--- calculatrice/src/main/java/ma/ehei/calculatrice/App.java
+++ calculatrice/src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,11 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+		Calculatrice c =new Calculatrice();
+		 
+		System.out.println(Calculatrice.multiplication(5,2));
+
 	}
+
+	 
 }
